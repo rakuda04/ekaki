5afad0e78850132374c4ea7bc60810648e62bcb3
rakuda04 <abdulkaderbaghaffar@gmail.com>
2023-09-05 16:57:08 +0300

    updated dynamic grid. next step is square hovering

--- ekaki.js
+++ ekaki.js
@@ -1,3 +1,11 @@
+let slider = document.getElementById('gridSize');
+
+slider.addEventListener('input', () => {
+    let sizeValue = parseInt(slider.value, 10);  // Convert string to integer
+    defaultEkakiBoard(sizeValue);
+});
+
+
 
 function defaultEkakiBoard(size) {
  let ekakiBoard = document.querySelector("#ekakiBoard");
@@ -6,21 +14,12 @@ function defaultEkakiBoard(size) {
 ekakiBoard.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
 ekakiBoard.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i <256; i++) {
+for (let i = 0; i <size * size; i++) {
 let square = document.createElement("div");
 square.style.backgroundColor = "magenta";
 ekakiBoard.insertAdjacentElement("beforeend", square); 
 }
 }
 
-defaultEkakiBoard(16)
-
-function changeSize(input){
-    if (input >= 2 || input <= 100){
-        defaultEkakiBoard(input);
-    }
-    else{
-        console.log("wonder what happends...");
-    }
-}
+// create a hover effect so when it is over a square a different color is showing and if the evenlister mousclick is clicked it changes the color
 
