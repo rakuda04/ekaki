a81d679afffc450072583d3b1574fddca3b45c70
rakuda04 <abdulkaderbaghaffar@gmail.com>
2023-09-10 10:06:01 +0300

    added reset button & wip color palette

--- ekaki.js
+++ ekaki.js
@@ -1,7 +1,7 @@
 let slider = document.getElementById('gridSize');
 
 slider.addEventListener('input', () => {
-    let sizeValue = parseInt(slider.value, 10);  // Convert string to integer
+    let sizeValue = parseInt(slider.value, 10); 
     defaultEkakiBoard(sizeValue);
 });
 
@@ -26,12 +26,17 @@ ekakiBoard.insertAdjacentElement("beforeend", square);
 }
 }
 
-// connect resetBoard to reset button
+
 function resetBoard() {
     let board = document.querySelector("#ekakiBoard");
     let squares = board.querySelectorAll("div");
-    squares.forEach((div) => (div.style.backgroundColor = "magenta"));
+    squares.forEach((div) => {
+        div.style.backgroundColor = "magenta";
+        div.classList.remove("active");
+    });
 }
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetBoard)
 
 let isDragging = false;
 
@@ -51,9 +56,35 @@ function handleMouseUp(){
 }
 
 function handleMouseLeave(){
-   
 }
 
+let colorpalette = document.getElementById("selectColorPalette");
+colorpalette.addEventListener("change", handleColorPalette);
+
+
+function handleColorPalette(){
+    let colorDisplay = document.getElementById("colorDisplay");
+    colorDisplay.innerHTML = '';
+    let selectedPaletteName = colorpalette.value;
+    let selectedColors = palettes[selectedPaletteName];
+    colorDisplay.style.gridTemplateColumns = "1fr 1fr";
+    colorDisplay.style.gridTemplateRows = "1fr 1fr";
+    for (let i = 0; i < selectedColors.length; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = selectedColors[i];
+        square.classList.add("colorSquare"); 
+        colorDisplay.appendChild(square);
+    }
+}
+
+
+const palettes = {
+    "hot flowers": ["#f9d5e5", "#eeac99", "#e06377", "#c83349"],
+    "cold flowers": ["#5b9aa0", "#d6d4e0", "#b8a9c9", "#622569"],
+    "funky flowers": ["##6b5b95", "#feb236", "#d64161", "#ff7b25"],
+    "grassy flowers": ["#3e4444", "#82b74b", "#405d27", "#c1946a"],
+    "kyukyurarin flowers": ["#d6cbd3", "#eca1a6","#bdcebe" ,"#ada397"],
+}
 // add changecolor fuction
 
 // add multiple colors to the board
